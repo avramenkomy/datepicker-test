db1a5c5e8d314f043d5d0a99b8c8cd9556f9a570
Mikhail Avramenko <avramenkomy@yandex.ru>
2021-10-14 13:29:08 +0600

    WIP: add style for dialog

--- src/elements/NewQuarterPicker.js
+++ src/elements/NewQuarterPicker.js
@@ -10,27 +10,34 @@ import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   root: {
+    // Стили для размера диалога
     '& .css-1t1j96h-MuiPaper-root-MuiDialog-paper': {
       minWidth: '310px',
       maxHeight: '100%',
     },
+    // Стили для секции заголовка диалога
     '& .css-bdhsul-MuiTypography-root-MuiDialogTitle-root': {
       background: '#00264D',
       color: 'rgba(255, 255, 255, 0.85)',
     },
-    '& .css-ueukts-MuiButtonBase-root-MuiToggleButton-root.Mui-selected': {
+    // Стили для выбранного квартала в секции контента диалога
+    '& .css-17ndd9v-MuiButtonBase-root-MuiToggleButton-root.Mui-selected': {
       background: '#00264D',
       color: 'rgba(255, 255, 255, 0.85)',
     },
-    '& .css-ueukts-MuiButtonBase-root-MuiToggleButton-root.Mui-selected:hover': {
+    '& .css-17ndd9v-MuiButtonBase-root-MuiToggleButton-root.Mui-selected:hover': {
       background: '#00264D'
-    }
+    },
+    '& css-1e6y48t-MuiButtonBase-root-MuiButton-root': {
+      color: '#00264D'
+    },
   },
-  dialog_root: {
-    minWidth: '310px',
-    maxHeight: '100%',
-  }
-
+  dialog_actions_button: {
+    color: '#00264D'
+  },
+  arrow_btn: {
+    color: 'rgba(255, 255, 255, 0.85)'
+  },
 });
 
 
@@ -79,43 +86,44 @@ function QuarterPicker() {
   return (
     <React.Fragment>
       <Dialog className={classes.root} open={openSelectQuarter} onClose={() => setOpenSelectQuarter(false)}>
-        <div>
         <DialogTitle>
           <Grid container direction="row" alignItems="center">
             <Grid item xs={2}>
-              <IconButton onClick={decrement}><ArrowBackIosIcon/></IconButton>
+              <IconButton className={classes.arrow_btn} color="inherit" onClick={decrement}><ArrowBackIosIcon/></IconButton>
             </Grid>
             <Grid item xs={8} textAlign="center">
               {cloneYear}
             </Grid>
             <Grid item xs={2}>
-              <IconButton onClick={increment}><ArrowForwardIosIcon/></IconButton>
+              <IconButton className={classes.arrow_btn} color="inherit" onClick={increment}><ArrowForwardIosIcon/></IconButton>
             </Grid>
           </Grid>
         </DialogTitle>
 
         <DialogContent dividers>
-        <ToggleButtonGroup
-            orientation="vertical"
-            value={ReportPeriodID}
-            exclusive
-            onChange={handleSetReportPeriodID}
-          >
-            {
-              quarterList.map(item => (
-                <ToggleButton key={item.id} value={item.id}>
-                  {item.quarterName}
-                </ToggleButton>
-              ))
-            }
-          </ToggleButtonGroup>
+          <Grid container xs={12}>
+            <ToggleButtonGroup
+              orientation="vertical"
+              value={ReportPeriodID}
+              fullWidth
+              exclusive
+              onChange={handleSetReportPeriodID}
+            >
+              {
+                quarterList.map(item => (
+                  <ToggleButton key={item.id} value={item.id}>
+                    {item.quarterName}
+                  </ToggleButton>
+                ))
+              }
+            </ToggleButtonGroup>
+          </Grid>
         </DialogContent>
 
         <DialogActions>
-          <Button onClick={() => setOpenSelectQuarter(false)}>Отмена</Button>
-          <Button onClick={handleSetReportPeriod}>Принять</Button>
+          <Button color="inherit" onClick={() => setOpenSelectQuarter(false)}>Отмена</Button>
+          <Button color="inherit" onClick={handleSetReportPeriod}>Принять</Button>
         </DialogActions>
-        </div>
       </Dialog>
 
       <TextField
